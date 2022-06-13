fa5c7925a6fdbbb1d3a2260d6905f0abff9c9277
serkankaragoz <serkankaragoz2002@gmail.com>
2022-06-13 21:08:51 +0300

    Constructed the boundaries of minesweeper field

--- src/MineSweeperFrame.java
+++ src/MineSweeperFrame.java
@@ -3,46 +3,31 @@ import java.awt.*;
 
 public class MineSweeperFrame extends JFrame{
     public MineSweeperFrame(){
-        this.setLayout(null);
+
         MineSweeperPanel p = new MineSweeperPanel();
+        this.setLayout(null);
+        this.add(p);
+
         p.setLayout(null);
-        p.setSize(100, 100);
-        p.setBounds(50,50, 300,300);
-        // p.paint();
         p.setBackground(Color.GREEN);
 
+        p.setBounds(p.BOX_SIZE,2*p.BOX_SIZE, p.FIELD_WIDTH,p.FIELD_HEIGHT);
 
-        this.add(p);
+
+        //p.setBounds(1,1, p.FIELD_WIDTH,p.FIELD_HEIGHT);
+
+
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
 
-
-        this.setBounds(200, 400, 400, 400);
         this.setResizable(false);
+        // The 17 and 40 numbers to make the minesweeper field symmetric since
+        this.setBounds(200, 400, p.FIELD_WIDTH + 2 * p.BOX_SIZE + 17, p.FIELD_HEIGHT + 3 * p.BOX_SIZE + 40);
+        System.out.println("Width: " + this.getBounds() + " Height: " + this.getAlignmentY());
+
+        //this.setBounds(200, 400, 200, 200);
+
+
         this.setVisible(true);
     }
-
-    /*
-    public MineSweeperFrame(int boxSize, int rowAmount, int colAmount){
-        this.setLayout(null);
-        MineSweeperPanel p = new MineSweeperPanel();
-        //p.setLayout(null);
-        p.setSize(100, 100);
-        p.setBounds(50,50, 300,300);
-        // p.paint();
-        p.setBackground(Color.GREEN);
-
-
-        this.add(p);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-
-
-        this.setBounds(200, 400, 600, 600);
-        this.setResizable(false);
-        this.setVisible(true);
-    }
-    */
 }
